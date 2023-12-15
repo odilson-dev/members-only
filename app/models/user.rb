73c014e5975e0e06b0ca88999960c00e41a565be
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-15 11:24:28 -0500

    Add User model, validations and migrations

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ApplicationRecord
+    validates :name, presence: true, length: { in: 4..12 }, uniqueness: true
+    validates :email, presence: true, uniqueness: true
+    validates :password, presence: true, length: { in: 6..16 }
+end
