3ff8d5d061805a6d867ac06ffbd3dd5c7ea145df
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-15 13:57:06 -0500

    Add device as authenticator tool

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
     validates :name, presence: true, length: { in: 4..12 }, uniqueness: true
     validates :email, presence: true, uniqueness: true
     validates :password, presence: true, length: { in: 6..16 }
