ed7562cf07dd1524ad04e55d0d0c4e56147d0e20
odilsonjs <italisodilsonwoodly@hotmail.com>
2023-12-16 11:17:58 -0500

    Set up Posts controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,36 @@
 class PostsController < ApplicationController
+    before_action :require_login only: [:new, :create]
+
+
+    def new
+        @post = Post.new
+    end
+
+    def create
+        @post = Post.new(post_params)
+
+        if @post.save
+            redirect_to new_post_path
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    def index
+        @posts = Post.all
+    end
+
+    private
+
+    def require_login
+      if current_user.logged_in?
+      # allow the user to perform the action they wanted
+      else
+        redirect_to login_path
+      end
+    end
+
+    def post_params
+        params.require(:post).permit(:message)
+    end
 end
